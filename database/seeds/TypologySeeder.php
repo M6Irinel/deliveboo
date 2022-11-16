4b0ef8f93cbc755a536d8c0e6b64c42cea97a4f1
Alaikum <carangiadamo@gmail.com>
2022-11-16 18:39:08 +0100

    add migration

--- database/seeds/TypologySeeder.php
+++ database/seeds/TypologySeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Typology;
 use Illuminate\Database\Seeder;
 
 class TypologySeeder extends Seeder
@@ -11,6 +12,68 @@ class TypologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $myTypologies = [
+            [
+                "Tipologia" => "Primo",
+                "Immagine" => "primo.png",
+            ],
+            [
+                "Tipologia" => "Sushi",
+                "Immagine" => "sushi.png",
+            ],
+            [
+                "Tipologia" => "Pizza",
+                "Immagine" => "pizza.png",
+            ],
+            [
+                "Tipologia" => "Poke",
+                "Immagine" => "poke.png",
+            ],
+            [
+                "Tipologia" => "Kebab",
+                "Immagine" => "kebab.png",
+            ],
+            [
+                "Tipologia" => "Burger",
+                "Immagine" => "burger.png",
+            ],
+            [
+                "Tipologia" => "Veg",
+                "Immagine" => "veg.png",
+            ],
+            [
+                "Tipologia" => "Pesce",
+                "Immagine" => "pesce.png",
+            ],
+            [
+                "Tipologia" => "Carne",
+                "Immagine" => "carne.png",
+            ],
+     
+          
+            [
+                "Tipologia" => "Indiano",
+                "Immagine" => "indiano.png",
+            ],
+            [
+                "Tipologia" => "Gourmet",
+                "Immagine" => "gourmet.png",
+            ],
+            [
+                "Tipologia" => "Dolci",
+                "Immagine" => "dolci.png",
+            ],
+            [
+                "Tipologia" => "Gelato",
+                "Immagine" => "gelato.png",
+            ],
+        ];
+
+        foreach ($myTypologies as $t) {
+            Typology::create([
+                "name" => $t["Tipologia"],
+                "image" => $t["Immagine"],
+            ]);
+        }
     }
 }

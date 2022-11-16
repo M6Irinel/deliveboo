a4aac68793a0bcb57752a3b73d8fdaaaf22273f1
Alaikum <carangiadamo@gmail.com>
2022-11-16 17:03:32 +0100

    restaurant user

--- database/migrations/2022_11_16_151621_create_restaurants_table.php
+++ database/migrations/2022_11_16_151621_create_restaurants_table.php
@@ -15,6 +15,7 @@ class CreateRestaurantsTable extends Migration
     {
         Schema::create('restaurants', function (Blueprint $table) {
             $table->id();
+            // $table->bigInteger('user_id');
             $table->timestamps();
         });
     }

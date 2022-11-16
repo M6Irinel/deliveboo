164e4346c9ef5fb683929b2deb076701648d4e7b
JacopoMarini <marinijacopo96@gmail.com>
2022-11-16 17:41:12 +0100

    Add migration fixes

--- app/Restaurant.php
+++ app/Restaurant.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Restaurant extends Model
 {
     public function user(){
-        return $this->belongsTo('App\User',);
+        return $this->belongsTo('App\User');
+    }
+
+    public function plates(){
+        return $this->hasMany('App\Plate');
     }
 }

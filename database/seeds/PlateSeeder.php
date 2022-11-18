040a32ae77072fd3089a3752fab100503fe8fa88
JacopoMarini <marinijacopo96@gmail.com>
2022-11-18 10:18:15 +0100

    Add plate seeder

--- database/seeds/PlateSeeder.php
+++ database/seeds/PlateSeeder.php
@@ -11,6 +11,26 @@ class PlateSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $plates = [
+            [
+                'plate_name' => 'Pasta alla carbonara',
+                'ingridients' => 'Uova, pecorino, guanciale, pasta all\'uovo',
+                'plate_description' => 'Piatto della tradizione romana',
+                'plate_price' => 15,
+                'visibility' => true,
+                'plate_image' => '',
+            ]
+        ];
+
+        foreach ($plates as $plate) {
+            Plate::create([
+                'plate_name'            => $plate['plate_name'],
+                'ingridients'           => $plate['ingridients'],
+                'plate_description'     => $plate['plate_description'],
+                'plate_price'           => $plate['plate_price'],
+                'visibility'            => $plate['visibility'],
+                'plate_image'           => $plate['plate_image'],
+            ]);
+        }
     }
 }

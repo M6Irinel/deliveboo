a6bee918413562557ff3799846f083da3ebb168d
JacopoMarini <marinijacopo96@gmail.com>
2022-11-18 18:11:41 +0100

    Fix restaurant seeder

--- database/seeds/RestaurantSeeder.php
+++ database/seeds/RestaurantSeeder.php
@@ -14,37 +14,37 @@ class RestaurantSeeder extends Seeder
     {
         $restaurants = [
             [
-                'restaurant_address'        => 'Via Dante Alighieri 212',
+                'restaurant_address'        => 'Via Dante 212',
                 'p_iva'                     => 'IT564623181',
                 'restaurant_description'    => 'Qui al ristorante da Gianluca potrai trovare un ambiente riservato e diversi tipi di cucina',
                 'restaurant_phone_number'   => '3254445876',
                 'restaurant_website'        => 'http//:ristorante-da-gianluca',
                 'restaurant_image'          => '',
             ],
-            // [
-            //     'restaurant_address'        => 'Via Milano 11/A',
-            //     'p_iva'                     => 'IT457895764',
-            //     'restaurant_description'    => 'Qui al ristorante da Mauro potrai trovare piatti che variano da quelli tipici della tradizione alla cucina fusion',
-            //     'restaurant_phone_number'   => '9876543210',
-            //     'restaurant_website'        => 'http//:ristorante-da-mauro',
-            //     'restaurant_image'          => '',
-            // ],
-            // [,
-            //     'restaurant_address'        => 'Viale Bonaria 153',
-            //     'p_iva'                     => 'IT961735498',
-            //     'restaurant_description'    => 'Ristorante da Alessandro propone un menù variegato per soddisfare ogni tipo di palato',
-            //     'restaurant_phone_number'   => '3405430191',
-            //     'restaurant_website'        => 'http//:ristorante-da-alessandro',
-            //     'restaurant_image'          => '',
-            // ],
-            // [
-            //     'restaurant_address'        => 'Corso Vittorio Emanuele II 15',
-            //     'p_iva'                     => 'IT457295745',
-            //     'restaurant_description'    => 'Da Massimo, ristorante di nuova apertura vi invita ad assaggiare le specialità della casa!',
-            //     'restaurant_phone_number'   => '3320985821',
-            //     'restaurant_website'        => 'http//:ristorante-da-massimo',
-            //     'restaurant_image'          => '',
-            // ],
+            [
+                'restaurant_address'        => 'Via Milano 11/A',
+                'p_iva'                     => 'IT457895764',
+                'restaurant_description'    => 'Qui al ristorante da Mauro potrai trovare piatti che variano da quelli tipici della tradizione alla cucina fusion',
+                'restaurant_phone_number'   => '9876543210',
+                'restaurant_website'        => 'http//:ristorante-da-mauro',
+                'restaurant_image'          => '',
+            ],
+            [
+                'restaurant_address'        => 'Viale Bonaria 153',
+                'p_iva'                     => 'IT961735498',
+                'restaurant_description'    => 'Ristorante da Alessandro propone un menù variegato per soddisfare ogni tipo di palato',
+                'restaurant_phone_number'   => '3405430191',
+                'restaurant_website'        => 'http//:ristorante-da-alessandro',
+                'restaurant_image'          => '',
+            ],
+            [
+                'restaurant_address'        => 'Corso Vittorio Emanuele II 15',
+                'p_iva'                     => 'IT457295745',
+                'restaurant_description'    => 'Da Massimo, ristorante di nuova apertura vi invita ad assaggiare le specialità della casa!',
+                'restaurant_phone_number'   => '3320985821',
+                'restaurant_website'        => 'http//:ristorante-da-massimo',
+                'restaurant_image'          => '',
+            ],
             
         ];
 

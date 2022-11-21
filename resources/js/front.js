e43c33ef727b86f0498d4bb71fc3729b1850d2eb
JacopoMarini <marinijacopo96@gmail.com>
2022-11-21 14:41:56 +0100

    Add vue router

---
+++ resources/js/front.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+Vue.use(VueRouter)
+
+const app = new Vue({
+    el: '#app',
+});
+

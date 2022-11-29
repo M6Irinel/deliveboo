d1afcc841e0537698a3baca4c388b9632c7b697d
Lucaponz91 <lucapo1991@msn.com>
2022-11-29 18:28:55 +0100

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Braintree\Gateway;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->singleton(Gateway::class, function($app){
+            return new Gateway(
+                [
+                    "environment" => "rpdz2h6dvbg4b3nb", 
+                    "merchantId" => "z3ndhvftb2mwzq2f",
+                    "publicKey" => "e6c6f1013149067ab94d5254cb831ba0"
+                ]
+                );
+        });
+
+        }
     }
-}
+
